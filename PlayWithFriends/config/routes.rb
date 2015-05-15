6167211042180d91af494c013fd3fbc7a95689e0
Taylor Becker <taylorjosephbecker@gmail.com>
2015-05-15 04:52:30 -0400

    Fixed a number of small bugs, added post routes for invites and reputation
    Fixed being able to send multiple invites to the same server and inviting to your own server
    Fixed a number of instances where a deleted server would make the site unusable(attempting to use nil server information)
    Added post/patch routes to be able to create/edit invites and reputations after switching to stricting defined routes

--- PlayWithFriends/config/routes.rb
+++ PlayWithFriends/config/routes.rb
@@ -13,10 +13,13 @@ Rails.application.routes.draw do
   get 'invites/:id' => 'invites#show', as: :invite
   get 'servers/:server_id/request' => 'invites#new', as: :invite_to_server
   get 'invites/:id/respond/:response' => 'invites#respond', as: :invite_response
+  post 'invites' => 'invites#create', as: :create_invite
 
   # reputation routes
   get 'reputations' => 'reputations#index', as: :reputations
   get 'reputations/:id' => 'reputations#show', as: :reputation
   get 'reputations/:id/edit' => 'reputations#edit', as: :edit_reputation
   get 'invites/:invite/rate' => 'reputations#new', as: :rate
+  post 'reputations' => 'reputations#create', as: :create_reputation
+  patch 'reputations/:id' => 'reputations#update', as: :update_reputation
 end
